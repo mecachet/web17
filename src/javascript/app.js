6b6390d6df84932e2224ed50b3000db8b584ed49
mecachet <otoqvilitaia@gmail.com>
2023-07-28 22:47:00 +0400

    added card with characters

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,10 +1,59 @@
 console.log("JavaScript file is linked correctly. Hooray.");
-function createImage() {
-  let img = document.createElement("img");
-  img.src = "src/images/poppies.jpg";
-  img.alt = "Poppie's Fish and Chips";
-  img.width = 800;
-  img.height = 400;
-  document.body.appendChild(img);
-  createImage();
-}
+const characters = [
+  {
+    first_name: "Harry",
+    last_name: "Potter",
+    house: "Gryffindor",
+    image: "http://hp-api.herokuapp.com/images/harry.jpg",
+  },
+  {
+    first_name: "Hermione",
+    last_name: "Granger",
+    house: "Gryffindor",
+    image: "http://hp-api.herokuapp.com/images/hermione.jpeg",
+  },
+  {
+    first_name: "Ron",
+    last_name: "Weasley",
+    house: "Gryffindor",
+    image: "http://hp-api.herokuapp.com/images/ron.jpg",
+  },
+  {
+    first_name: "Draco",
+    last_name: "Malfoy",
+    house: "Slytherin",
+    image: "http://hp-api.herokuapp.com/images/draco.jpg",
+  },
+];
+document.addEventListener("DOMContentLoaded", () => {
+  const charactersListElement = document.getElementById("characters-list");
+  characters.forEach((character) => {
+    const characterCard = document.createElement("div");
+    characterCard.classList.add("character-card");
+
+    const characterInfo = document.createElement("div");
+    characterInfo.classList.add("character-info");
+
+    const characterName = document.createElement("div");
+    characterName.classList.add("character-name");
+    characterName.textContent = `${character.first_name} ${character.last_name}`;
+
+    const characterHouse = document.createElement("div");
+    characterHouse.classList.add("character-house");
+    characterHouse.textContent = character.house;
+
+    characterInfo.appendChild(characterName);
+    characterInfo.appendChild(characterHouse);
+
+    characterCard.appendChild(characterInfo);
+
+    const characterImage = document.createElement("img");
+    characterImage.classList.add("character-image");
+    characterImage.src = character.image;
+    characterImage.alt = `${character.first_name} ${character.last_name}`;
+
+    characterCard.appendChild(characterImage);
+
+    charactersListElement.appendChild(characterCard);
+  });
+});
